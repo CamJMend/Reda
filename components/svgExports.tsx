314268ff417f248993a83b61561088bc140352f7
Alexme116 <alexmoncada16@hotmail.com>
2024-11-06 13:19:45 -0600

    Forms SVG Changed

---
+++ components/svgExports.tsx
@@ -0,0 +1,20 @@
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+
+export const HandShakeIcon = (props : any) => {
+    return <FontAwesome6 name="handshake" {... props} />
+};
+
+export const ArrowRightIcon = (props : any) => {
+    return <AntDesign name="arrowright" {... props} />
+};
+
+export const HandHoldingIcon = (props : any) => {
+    return <FontAwesome6 name="hand-holding" {... props} />
+};
+
+export const HandUpIcon = (props : any) => {
+    return <Entypo name="hand" {... props} />
+};
+
